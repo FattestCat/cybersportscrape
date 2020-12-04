bec34ccac7effaefe4a4d202d7f6204c292414d1
Qot <podkovinsky.s@gmail.com>
2020-12-04 13:25:32 +0300

    Upd: be(reformatted models)

--- backend/PostData/models.py
+++ backend/PostData/models.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from django.utils.timezone import now
 from django.core.exceptions import ObjectDoesNotExist
 import pymorphy2
+import string
 
 morph = pymorphy2.MorphAnalyzer()
 
@@ -17,13 +18,13 @@ class Post(models.Model):
         # pylint: disable=no-member
         super().save(*args, **kwargs)
         for word in self.content.lower().split():
+            wd = morph.parse(word)[0].normal_form
+            wd = wd.translate(str.maketrans('','',string.punctuation))
             try:
-                wd = morph.parse(word)[0].normal_form
                 word_in_db = Word.objects.get(word=wd)
                 word_in_db.count = word_in_db.count + 1
                 word_in_db.save(update_fields=['count'])
             except ObjectDoesNotExist:
-                wd = morph.parse(word)[0].normal_form
                 w = Word(word=wd, count=1)
                 w.save()
 
